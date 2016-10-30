66a5ac564e8c9ed9a4cac7aae79d95a3b6060b97
allanaz <gzych.allan@gmail.com>
2016-10-29 21:14:16 -0700

    return clowns

--- app/src/main/java/com/mvhs/sdconnected/VotingLocationResults.java
+++ app/src/main/java/com/mvhs/sdconnected/VotingLocationResults.java
@@ -1,13 +1,26 @@
 package com.mvhs.sdconnected;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+
+
 
 public class VotingLocationResults extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(VotingLocationSearch.EXTRA_MESSAGE);
         setContentView(R.layout.activity_voting_location_results);
+        TextView address = (TextView) findViewById(R.id.textView4);
+        address.setText(message);
+
+
     }
 }
+
+
--- app/src/main/java/com/mvhs/sdconnected/VotingLocationSearch.java
+++ app/src/main/java/com/mvhs/sdconnected/VotingLocationSearch.java
@@ -2,12 +2,25 @@ package com.mvhs.sdconnected;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.content.Intent;
+import android.view.View;
+
 
 public class VotingLocationSearch extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = "com.mvhs.sdconnected";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voting_location_search);
+    }
+
+    public void searchButton(View button){
+        Intent intent = new Intent(this, VotingLocationResults.class);
+        EditText addressField = (EditText) findViewById(R.id.editText2);
+        String string = addressField.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, string);
+        startActivity(intent);
     }
 }
