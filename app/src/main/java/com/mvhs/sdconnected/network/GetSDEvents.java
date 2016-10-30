9c49a3eadf33826df7a42fbffd324d61750474b4
Andrew Yates <andrewmacu@me.com>
2016-10-30 02:44:26 -0700

    Add dates

--- app/src/main/java/com/mvhs/sdconnected/network/GetSDEvents.java
+++ app/src/main/java/com/mvhs/sdconnected/network/GetSDEvents.java
@@ -46,22 +46,33 @@ public class GetSDEvents extends AsyncTask<String, Integer, Boolean> {
                     String desc = eventObj.getString("event_desc");
                     String loc = eventObj.getString("event_loc");
                     String type = eventObj.getString("event_type");
-                    String start = eventObj.getString("event_start");
-                    String end = eventObj.getString("event_end");
+                    String start = eventObj.getString("event_start").split("T")[0];
+                    String startTime = eventObj.getString("event_start").split("T")[1];
+                    String end = eventObj.getString("event_end").split("T")[0];
+                    String endTime = eventObj.getString("event_end").split("T")[1];
                     String host = eventObj.getString("event_host");
                     String url = eventObj.getString("event_url");
                     //int att = Integer.parseInt(eventObj.getString("exp_attendance").replaceAll(",", ""));
                     //int partips = Integer.parseInt(eventObj.getString("exp_participants").replace(",", ""));
                     String address = eventObj.getString("event_address");
 
-                    //DateFormat df = new SimpleDateFormat("yyyy-MM-ddTHH:mm:ss");
-                    //Date startDate = df.parse(start);
-                    //Date endDate = df.parse(end);
+                    DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+                    Date startDate = df.parse(start);
+                    Date endDate = df.parse(end);
 
-                    //Date startDate = Date.valueOf(start);
-                    //Date endDate = Date.valueOf(end);
+                    DateFormat df2 = new SimpleDateFormat("HH:mm:ss");
+                    Date startTimeD = df2.parse(startTime);
+                    Date endTimeD = df2.parse(endTime);
 
-                    SDConnected.sdevents.add(new SDEvent(id, title, subtitle, desc, loc, type, null, null, host, url, 0, 0, address, 0, 0));
+                    startDate.setHours(startTimeD.getHours());
+                    startDate.setMinutes(startTimeD.getMinutes());
+                    startDate.setSeconds(startTimeD.getSeconds());
+
+                    endDate.setHours(endTimeD.getHours());
+                    endDate.setMinutes(endTimeD.getMinutes());
+                    endDate.setSeconds(endTimeD.getSeconds());
+
+                    SDConnected.sdevents.add(new SDEvent(id, title, subtitle, desc, loc, type, startDate, endDate, host, url, 0, 0, address, 0, 0));
                     //Log.d(SDConnected.APP_NAME, SDConnected.sdevents.get(i).getStart().toString());
                 }
                 return true;
