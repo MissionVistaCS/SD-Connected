3be623901c95de9ac0b0f80cc691e651c85ebf26
LukeJHarvey <wxisxawesome@gmail.com>
2016-10-29 21:43:24 -0700

    fixed menus

--- app/src/main/java/com/mvhs/sdconnected/SDConnected.java
+++ app/src/main/java/com/mvhs/sdconnected/SDConnected.java
@@ -1,5 +1,6 @@
 package com.mvhs.sdconnected;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -34,14 +35,6 @@ public class SDConnected extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -102,16 +95,14 @@ public class SDConnected extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
-
+            Intent i = new Intent(this,SearchEvents.class);
+            startActivity(i);
         } else if (id == R.id.nav_slideshow) {
-
+            Intent i = new Intent(this,VotingLocationSearch.class);
+            startActivity(i);
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
