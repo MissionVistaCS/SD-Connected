9cf94f6e6fd2d64c4e52b59bf1381191cd7994f5
LukeJHarvey <wxisxawesome@gmail.com>
2016-10-29 22:57:31 -0700

    Getting Everything all together

--- app/src/main/java/com/mvhs/sdconnected/EventList.java
+++ app/src/main/java/com/mvhs/sdconnected/EventList.java
@@ -2,13 +2,33 @@ package com.mvhs.sdconnected;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class EventList extends AppCompatActivity {
+
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
         String s = getIntent().getDataString();
+
+        adapter=new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item,
+                listItems);
+
+    }
+
+    public void addItems(View v) {
+
     }
 }
--- app/src/main/java/com/mvhs/sdconnected/SearchEvents.java
+++ app/src/main/java/com/mvhs/sdconnected/SearchEvents.java
@@ -1,13 +1,31 @@
 package com.mvhs.sdconnected;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class SearchEvents extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SearchEvents extends AppCompatActivity implements View.OnClickListener {
+
+    //static ArrayList<SDEvent> list2 = new Arraylist<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_events);
+
+        final Button button = (Button) findViewById(R.id.Search);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, EventInfo.class);
+        startActivity(i);
     }
 }
